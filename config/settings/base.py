2232013808b0af7f61dd88d3b48335d332b55a9b
Jan Schmitz <jan.schmitz@whu.edu>
2026-05-13 13:26:12 +0200

    auto-build Tailwind in Docker dev so styles load on any host

--- config/settings/base.py
+++ config/settings/base.py
@@ -151,7 +151,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STORAGES = {
